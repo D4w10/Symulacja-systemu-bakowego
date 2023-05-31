d3962d117ab50fea1234727376836ea5e94b0833
Mateusz Kocoń <mateusz589@onet.pl>
2023-05-31 12:40:49 +0200

    money_transfer_to_saveacc

--- Bank1/routes/pages.js
+++ Bank1/routes/pages.js
@@ -337,6 +337,12 @@ router.post('/create_savings_account', authController.isLoggedIn, (req, res) => 
 
 
 
+
+
+
+
+
+
 router.post('/przelej-srodki', authController.isLoggedIn, (req, res) => {
   const userId = req.userid.id; // Pobranie identyfikatora zalogowanego użytkownika z sesji
 
@@ -404,6 +410,558 @@ router.post('/przelej-srodki', authController.isLoggedIn, (req, res) => {
     });
   });
 });
+
+
+
+
+
+router.post('/wyplac-srodki', (req, res) => {
+  const accountId = req.body.account_id;
+
+  // Pobranie danych konta oszczędnościowego
+  db.query('SELECT * FROM k_oscz WHERE id_oszcz = ?', [accountId], (error, results) => {
+    if (error) {
+      console.error('Błąd zapytania SQL: ', error);
+      return res.status(500).send('Wystąpił błąd podczas wypłacania środków.');
+    }
+
+    if (results.length === 0) {
+      return res.status(404).send('Nie znaleziono konta oszczędnościowego.');
+    }
+
+    const accountBalance = results[0].wplacone_srodki;
+    const accountId = results[0].id_account;
+
+    // Rozpoczęcie transakcji
+    db.beginTransaction((error) => {
+      if (error) {
+        console.error('Błąd rozpoczynania transakcji: ', error);
+        return res.status(500).send('Wystąpił błąd podczas wypłacania środków.');
+      }
+
+      // Aktualizacja stanu konta oszczędnościowego
+      db.query('UPDATE k_oscz SET wplacone_srodki = 0 WHERE id_oszcz = ?', [accountId], (error) => {
+        if (error) {
+          console.error('Błąd zapytania SQL: ', error);
+          return db.rollback(() => {
+            res.status(500).send('Wystąpił błąd podczas wypłacania środków.');
+          });
+        }
+
+        // Pobranie aktualnego bilansu konta głównego
+        db.query('SELECT bilans FROM account WHERE id_account = ?', [accountId], (error, results) => {
+          if (error) {
+            console.error('Błąd zapytania SQL: ', error);
+            return db.rollback(() => {
+              res.status(500).send('Wystąpił błąd podczas pobierania bilansu konta.');
+            });
+          }
+
+          if (results.length === 0) {
+            return db.rollback(() => {
+              res.status(404).send('Nie znaleziono konta głównego.');
+            });
+          }
+
+          const currentBalance = results[0].bilans;
+          const updatedBalance = currentBalance + accountBalance;
+
+          // Aktualizacja bilansu na koncie głównym
+          db.query('UPDATE account SET bilans = ? WHERE id_account = ?', [updatedBalance, accountId], (error) => {
+            if (error) {
+              console.error('Błąd zapytania SQL: ', error);
+              return db.rollback(() => {
+                res.status(500).send('Wystąpił błąd podczas aktualizacji bilansu konta.');
+              });
+            }
+
+            // Odejmowanie środków z konta oszczędnościowego
+            db.query('UPDATE k_oscz SET wplacone_srodki = wplacone_srodki - ? WHERE id_account = ?', [accountBalance, accountId], (error) => {
+              if (error) {
+                console.error('Błąd zapytania SQL: ', error);
+                return db.rollback(() => {
+                  res.status(500).send('Wystąpił błąd podczas odejmowania środków z konta oszczędnościowego.');
+                });
+              }
+
+              // Zatwierdzenie transakcji
+              db.commit((error) => {
+                if (error) {
+                  console.error('Błąd zatwierdzania transakcji: ', error);
+                  return db.rollback(() => {
+                    res.status(500).send('Wystąpił błąd podczas zatwierdzania transakcji.');
+                  });
+                }
+
+                return res.redirect('/test');
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// router.post('/wyplac-srodki', (req, res) => {
+//   const accountId = req.body.account_id;
+
+//   // Pobranie danych konta oszczędnościowego
+//   db.query('SELECT * FROM k_oscz WHERE id_oszcz = ?', [accountId], (error, results) => {
+//     if (error) {
+//       console.error('Błąd zapytania SQL: ', error);
+//       return res.status(500).send('Wystąpił błąd podczas wypłacania środków.');
+//     }
+
+//     if (results.length === 0) {
+//       return res.status(404).send('Nie znaleziono konta oszczędnościowego.');
+//     }
+
+//     const accountBalance = results[0].wplacone_srodki;
+//     const accountId = results[0].id_account;
+
+//     // Rozpoczęcie transakcji
+//     db.beginTransaction((error) => {
+//       if (error) {
+//         console.error('Błąd rozpoczynania transakcji: ', error);
+//         return res.status(500).send('Wystąpił błąd podczas wypłacania środków.');
+//       }
+
+//       // Aktualizacja stanu konta oszczędnościowego
+//       db.query('UPDATE k_oscz SET wplacone_srodki = 0 WHERE id_oszcz = ?', [accountId], (error) => {
+//         if (error) {
+//           console.error('Błąd zapytania SQL: ', error);
+//           return db.rollback(() => {
+//             res.status(500).send('Wystąpił błąd podczas wypłacania środków.');
+//           });
+//         }
+
+//         // Pobranie aktualnego bilansu konta głównego
+//         db.query('SELECT bilans FROM account WHERE id_account = ?', [accountId], (error, results) => {
+//           if (error) {
+//             console.error('Błąd zapytania SQL: ', error);
+//             return db.rollback(() => {
+//               res.status(500).send('Wystąpił błąd podczas pobierania bilansu konta.');
+//             });
+//           }
+        
+//           if (results.length === 0) {
+//             return db.rollback(() => {
+//               res.status(404).send('Nie znaleziono konta głównego.');
+//             });
+//           }
+        
+//           const currentBalance = results[0].bilans;
+//           const updatedBalance = currentBalance + accountBalance;
+        
+//           // Aktualizacja bilansu na koncie głównym
+//           db.query('UPDATE account SET bilans = ? WHERE id_account = ?', [updatedBalance, accountId], (error) => {
+//             if (error) {
+//               console.error('Błąd zapytania SQL: ', error);
+//               return db.rollback(() => {
+//                 res.status(500).send('Wystąpił błąd podczas aktualizacji bilansu konta.');
+//               });
+//             }
+        
+//             // Zatwierdzenie transakcji
+//             db.commit((error) => {
+//               if (error) {
+//                 console.error('Błąd zatwierdzania transakcji: ', error);
+//                 return db.rollback(() => {
+//                   res.status(500).send('Wystąpił błąd podczas zatwierdzania transakcji.');
+//                 });
+//               }
+        
+//               return res.redirect('/test');
+//                         });
+//                       });
+//                     });
+//                   });
+//                 });
+//               });
+//             });
+              
+
+
+
+
+
+
+
+
+
+// // Obsługa żądania wypłaty środków z konta oszczędnościowego
+// router.post('/wyplac-srodki', (req, res) => {
+//   const accountId = req.body.account_id;
+
+//   // Pobranie danych konta oszczędnościowego
+//   db.query('SELECT * FROM k_oscz WHERE id_oszcz = ?', accountId, (error, results) => {
+//     if (error) {
+//       console.error('Błąd zapytania SQL: ', error);
+//       return res.status(500).send('Wystąpił błąd podczas wypłacania środków.');
+//     }
+
+//     if (results.length === 0) {
+//       return res.status(404).send('Nie znaleziono konta oszczędnościowego.');
+//     }
+
+//     const accountBalance = results[0].wplacone_srodki;
+//     const userId = results[0].user_id;
+
+//     // Rozpoczęcie transakcji
+//     db.beginTransaction((error) => {
+//       if (error) {
+//         console.error('Błąd rozpoczynania transakcji: ', error);
+//         return res.status(500).send('Wystąpił błąd podczas wypłacania środków.');
+//       }
+
+//       // Aktualizacja stanu konta oszczędnościowego
+//       db.query('UPDATE k_oscz SET wplacone_srodki = 0 WHERE id_oszcz = ?', accountId, (error) => {
+//         if (error) {
+//           console.error('Błąd zapytania SQL: ', error);
+//           return db.rollback(() => {
+//             res.status(500).send('Wystąpił błąd podczas wypłacania środków.');
+//           });
+//         }
+
+//         // Pobranie aktualnego bilansu konta głównego
+//         db.query('SELECT bilans FROM account WHERE id_account = ?', userId, (error, results) => {
+//           if (error) {
+//             console.error('Błąd zapytania SQL: ', error);
+//             return db.rollback(() => {
+//               res.status(500).send('Wystąpił błąd podczas pobierania bilansu konta.');
+//             });
+//           }
+
+//           const currentBalance = results[0].bilans;
+//           const updatedBalance = currentBalance + accountBalance;
+
+//           // Aktualizacja bilansu na koncie głównym
+//           db.query('UPDATE account SET bilans = ? WHERE id_account = ?', [updatedBalance, userId], (error) => {
+//             if (error) {
+//               console.error('Błąd zapytania SQL: ', error);
+//               return db.rollback(() => {
+//                 res.status(500).send('Wystąpił błąd podczas aktualizacji bilansu konta.');
+//               });
+//             }
+
+//             // Zatwierdzenie transakcji
+//             db.commit((error) => {
+//               if (error) {
+//                 console.error('Błąd zatwierdzania transakcji: ', error);
+//                 return db.rollback(() => {
+//                   res.status(500).send('Wystąpił blad');
+                
+//               });
+//             }
+
+//             return res.redirect('/test');
+//           });
+//         });
+//       });
+//     });
+//   });
+// });
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// router.post('/wyplac-srodki', (req, res) => {
+//   const accountId = req.body.account_id;
+
+//   // Pobranie danych konta oszczędnościowego
+//   db.query('SELECT * FROM k_oscz WHERE id_oszcz = ?', accountId, (error, results) => {
+//     if (error) {
+//       console.error('Błąd zapytania SQL: ', error);
+//       return res.status(500).send('Wystąpił błąd podczas wypłacania środków.');
+//     }
+
+//     if (results.length === 0) {
+//       return res.status(404).send('Nie znaleziono konta oszczędnościowego.');
+//     }
+
+//     const accountBalance = results[0].wplacone_srodki;
+//     const userId = results[0].user_id;
+
+//     // Rozpoczęcie transakcji
+//     db.beginTransaction((error) => {
+//       if (error) {
+//         console.error('Błąd rozpoczynania transakcji: ', error);
+//         return res.status(500).send('Wystąpił błąd podczas wypłacania środków.');
+//       }
+
+//       // Aktualizacja stanu konta oszczędnościowego
+//       db.query('UPDATE k_oscz SET wplacone_srodki = 0 WHERE id_oszcz = ?', accountId, (error) => {
+//         if (error) {
+//           console.error('Błąd zapytania SQL: ', error);
+//           return db.rollback(() => {
+//             res.status(500).send('Wystąpił błąd podczas wypłacania środków.');
+//           });
+//         }
+
+//         // Aktualizacja bilansu na koncie głównym
+//         db.query('UPDATE account SET bilans = bilans + ? WHERE id_account = ?', [accountBalance, userId], (error) => {
+//           if (error) {
+//             console.error('Błąd zapytania SQL: ', error);
+//             return db.rollback(() => {
+//               res.status(500).send('Wystąpił błąd podczas aktualizacji bilansu konta.');
+//             });
+//           }
+
+//           // Zatwierdzenie transakcji
+//           db.commit((error) => {
+//             if (error) {
+//               console.error('Błąd zatwierdzania transakcji: ', error);
+//               return db.rollback(() => {
+//                 res.status(500).send('Wystąpił błąd podczas wypłacania środków.');
+//               });
+//             }
+
+//             return res.redirect('/test');
+//           });
+//         });
+//       });
+//     });
+//   });
+// });
+
+
+
+
+
+
+// router.post('/wyplac-srodki', (req, res) => {
+//   const accountId = req.body.account_id;
+
+//   // Pobranie danych konta oszczędnościowego
+//   db.query('SELECT * FROM k_oscz WHERE id_oszcz = ?', accountId, (error, results) => {
+//     if (error) {
+//       console.error('Błąd zapytania SQL: ', error);
+//       return res.status(500).send('Wystąpił błąd podczas wypłacania środków.');
+//     }
+
+//     if (results.length === 0) {
+//       return res.status(404).send('Nie znaleziono konta oszczędnościowego.');
+//     }
+
+//     const accountBalance = results[0].wplacone_srodki;
+//     const userId = results[0].user_id;
+
+//     // Rozpoczęcie transakcji
+//     db.beginTransaction((error) => {
+//       if (error) {
+//         console.error('Błąd rozpoczynania transakcji: ', error);
+//         return res.status(500).send('Wystąpił błąd podczas wypłacania środków.');
+//       }
+
+//       // Aktualizacja stanu konta oszczędnościowego
+//       db.query('UPDATE k_oscz SET wplacone_srodki = 0 WHERE id_oszcz = ?', accountId, (error) => {
+//         if (error) {
+//           console.error('Błąd zapytania SQL: ', error);
+//           return db.rollback(() => {
+//             res.status(500).send('Wystąpił błąd podczas wypłacania środków.');
+//           });
+//         }
+
+//         // Aktualizacja bilansu na koncie głównym
+//         db.query('UPDATE account SET bilans = bilans + ? WHERE user_id = ?', [accountBalance, userId], (error) => {
+//           if (error) {
+//             console.error('Błąd zapytania SQL: ', error);
+//             return db.rollback(() => {
+//               res.status(500).send('Wystąpił błąd podczas aktualizacji bilansu konta.');
+//             });
+//           }
+
+//           // Zatwierdzenie transakcji
+//           db.commit((error) => {
+//             if (error) {
+//               console.error('Błąd zatwierdzania transakcji: ', error);
+//               return db.rollback(() => {
+//                 res.status(500).send('Wystąpił błąd podczas wypłacania środków.');
+//               });
+//             }
+
+//             return res.redirect('/test');
+//           });
+//         });
+//       });
+//     });
+//   });
+// });
+
+
+
+
+
+
+
+// // Obsługa żądania wypłaty środków z konta oszczędnościowego
+// router.post('/wyplac-srodki', (req, res) => {
+//   const accountId = req.body.account_id;
+
+//   // Pobranie danych konta oszczędnościowego
+//   db.query('SELECT * FROM k_oscz WHERE id_oszcz = ?', accountId, (error, results) => {
+//     if (error) {
+//       console.error('Błąd zapytania SQL: ', error);
+//       return res.status(500).send('Wystąpił błąd podczas wypłacania środków.');
+//     }
+
+//     if (results.length === 0) {
+//       return res.status(404).send('Nie znaleziono konta oszczędnościowego.');
+//     }
+
+//     const accountBalance = results[0].wplacone_srodki;
+//     const userId = results[0].user_id;
+
+//     // Aktualizacja stanu konta oszczędnościowego
+//     db.query('UPDATE k_oscz SET wplacone_srodki = 0 WHERE id_oszcz = ?', accountId, (error) => {
+//       if (error) {
+//         console.error('Błąd zapytania SQL: ', error);
+//         return res.status(500).send('Wystąpił błąd podczas wypłacania środków.');
+//       }
+
+//       // Aktualizacja bilansu na koncie głównym
+//       db.query('UPDATE account SET bilans = bilans + ? WHERE id_account = ?', [accountBalance, userId], (error) => {
+//         if (error) {
+//           console.error('Błąd zapytania SQL: ', error);
+//           return res.status(500).send('Wystąpił błąd podczas aktualizacji bilansu konta.');
+//         }
+
+//         return res.redirect('/test');
+//       });
+//     });
+//   });
+// });
+
+
+// router.post('/wyplac-srodki', (req, res) => {
+//   const accountId = req.body.account_id;
+
+//   // Pobranie danych konta oszczędnościowego
+//   db.query('SELECT * FROM k_oscz WHERE id_oszcz = ?', accountId, (error, results) => {
+//     if (error) {
+//       console.error('Błąd zapytania SQL: ', error);
+//       return res.status(500).send('Wystąpił błąd podczas wypłacania środków.');
+//     }
+
+//     if (results.length === 0) {
+//       return res.status(404).send('Nie znaleziono konta oszczędnościowego.');
+//     }
+
+//     const accountBalance = results[0].wplacone_srodki;
+
+//     // Aktualizacja stanu konta oszczędnościowego
+//     db.query('UPDATE k_oscz SET wplacone_srodki = 0 WHERE id_oszcz = ?', accountId, (error) => {
+//       if (error) {
+//         console.error('Błąd zapytania SQL: ', error);
+//         return res.status(500).send('Wystąpił błąd podczas wypłacania środków.');
+//       }
+
+//       // Aktualizacja bilansu na koncie głównym
+//       db.query('UPDATE account SET bilans = bilans + ? WHERE id_account = ?', [accountBalance, accountId], (error) => {
+//         if (error) {
+//           console.error('Błąd zapytania SQL: ', error);
+//           return res.status(500).send('Wystąpił błąd podczas aktualizacji bilansu konta.');
+//         }
+
+//         return res.redirect('/test');
+//       });
+//     });
+//   });
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Obsługa żądania wypłaty środków z konta oszczędnościowego
+// router.post('/wyplac-srodki', (req, res) => {
+//   const accountId = req.body.account_id;
+
+//   // Pobranie danych konta oszczędnościowego
+//   db.query('SELECT * FROM k_oscz WHERE id_oszcz = ?', accountId, (error, results) => {
+//     if (error) {
+//       console.error('Błąd zapytania SQL: ', error);
+//       return res.status(500).send('Wystąpił błąd podczas wypłacania środków.');
+//     }
+
+//     if (results.length === 0) {
+//       return res.status(404).send('Nie znaleziono konta oszczędnościowego.');
+//     }
+
+//     const accountBalance = results[0].wplacone_srodki;
+
+//     // Aktualizacja stanu konta oszczędnościowego
+//     db.query('UPDATE k_oscz SET wplacone_srodki = 0 WHERE id_oszcz = ?', accountId, (error) => {
+//       if (error) {
+//         console.error('Błąd zapytania SQL: ', error);
+//         return res.status(500).send('Wystąpił błąd podczas wypłacania środków.');
+//       }
+
+//       return res.redirect('/test');
+//     });
+//   });
+// });
+
+
+
+
 
 // router.post('/przelej-srodki', authController.isLoggedIn, (req, res) => {
 //   const userId = req.userid.id; // Pobranie identyfikatora zalogowanego użytkownika z sesji
